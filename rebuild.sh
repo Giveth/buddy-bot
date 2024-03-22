afe71b121dbefe59c3acbf3472c1de559ceece20
geleeroyale <krassvs@gmail.com>
2024-03-22 15:47:41 +0100

    add newline after echo in service script

--- rebuild.sh
+++ rebuild.sh
@@ -7,12 +7,16 @@ NC='\033[0m'
 
 echo -e "${MAGENTA}1 - Kill and remove the docker container called buddy-bot${NC}"
 docker rm -f buddy-bot
+echo ""
 
 echo -e "${MAGENTA}2 - Pull the latest changes from the repo${NC}"
 git pull
+echo ""
 
 echo -e "${MAGENTA}3 - Build a new container called buddy-bot${NC}"
 docker build -t buddy-bot .
+echo ""
 
 echo -e "${MAGENTA}4 - Run the just built container as buddy-bot with restart-always${NC}"
 docker run -d --restart=always --name buddy-bot buddy-bot
+echo ""
