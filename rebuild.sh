0cea49eea4d9a10cef07d3f5338a6998edfc4e50
geleeroyale <krassvs@gmail.com>
2024-03-22 15:46:40 +0100

    add color to service script

--- rebuild.sh
+++ rebuild.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-echo 1 - Kill and remove the docker container called buddy-bot
+# ANSI escape code for magenta
+MAGENTA='\033[0;35m'
+# ANSI escape code to reset color
+NC='\033[0m'
+
+echo -e "${MAGENTA}1 - Kill and remove the docker container called buddy-bot${NC}"
 docker rm -f buddy-bot
 
-echo 2 - Pull the latest changes from the repo
+echo -e "${MAGENTA}2 - Pull the latest changes from the repo${NC}"
 git pull
 
-echo 3 - Build a new container called buddy-bot
+echo -e "${MAGENTA}3 - Build a new container called buddy-bot${NC}"
 docker build -t buddy-bot .
 
-echo 4 - Run the just built container as buddy-bot with restart-always
+echo -e "${MAGENTA}4 - Run the just built container as buddy-bot with restart-always${NC}"
 docker run -d --restart=always --name buddy-bot buddy-bot
