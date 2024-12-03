98a7d8efa737bb18c5c8fa1dc3cbf5ef46659770
geleeroyale <krassvs@gmail.com>
2024-12-03 20:41:15 +0100

    fix syntax error

--- src/eventHandlers.js
+++ src/eventHandlers.js
@@ -179,8 +179,10 @@ async function handleMessages(message) {
       );
     }
 
-    if (message.content.startsWith("!adminFeedback") &&
-      (ADMIN_IDS.includes(message.author.id)) {
+    if (
+      message.content.startsWith("!adminFeedback") &&
+      ADMIN_IDS.includes(message.author.id)
+    ) {
       const args = message.content.split(" ");
       const feedbackForUsername = args[1]; // The user that the feedback is for
 
@@ -218,8 +220,10 @@ async function handleMessages(message) {
       );
     }
 
-    if (message.content.startsWith("!leaderFeedback") &&
-      (ADMIN_IDS.includes(message.author.id)) {
+    if (
+      message.content.startsWith("!leaderFeedback") &&
+      ADMIN_IDS.includes(message.author.id)
+    ) {
       const args = message.content.split(" ");
       const feedbackForUsername = args[1]; // The user that the feedback is for
 
